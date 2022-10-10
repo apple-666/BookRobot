e2baf4ec4ac5cb085876984d2e5abe7b929eb7e0
apple <1446727937@qq.com>
2022-10-10 20:56:20 +0800

    add new ui

--- BookRobot/GUI_v2.py
+++ BookRobot/GUI_v2.py
@@ -1,34 +1,65 @@
 # @Time: 2022/8/22 21:48
 # @Author: DoubleApple
 
-from PySide2.QtWidgets import QApplication,QMessageBox
+from PySide2.QtWidgets import QApplication, QMessageBox, QDateTimeEdit
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtCore import QFile
+from PySide2.QtCore import QFile, QDateTime
 import AutoBook_v2 as autobook
 
 
+user_date = ''
+user_type = ''
+user_count = 2
+
 class RobotUi:
     def __init__(self):
-        # qfile_state = QFile("D:/apple/workspace/BookRobot/BookRobot/ui/robot.ui")
-        qfile_state = QFile("ui/robot.ui")
+        global user_date
+        # qfile = QFile("D:/apple/workspace/BookRobot/BookRobot/ui/robot.ui")
+        qfile = QFile("ui/robot.ui")
+        # qfile = QFile("ui_save/robot_t.ui")
+        qfile.open(QFile.ReadOnly)
+        qfile.close()
+        self.ui = QUiLoader().load(qfile)  # 界面对象
 
-        qfile_state.open(QFile.ReadOnly)
-        qfile_state.close()
-        self.ui = QUiLoader().load(qfile_state)  # 界面对象
-        self.ui.pushButton.clicked.connect(self.order)
+        # 初始化按钮选项
+        self.ui.radioButton_type2.setChecked(True)
+        self.ui.radioButton_count2.setChecked(True)
+        self.ui.dateEdit.setDateTime(QDateTime.currentDateTime())
+
+        # 获取选项信息
+        # 1 date
+        self.ui.dateEdit.dateChanged.connect(self.getChangingDate)
+        # 2 球场类型
+        self.ui.radioButton_type1.clicked.connect(lambda: self.getChangingType(self.ui.radioButton_type1))
+        self.ui.radioButton_type2.clicked.connect(lambda: self.getChangingType(self.ui.radioButton_type2))
+        # 3 球场个数
+        self.ui.radioButton_count1.clicked.connect(lambda: self.getChangingCount(self.ui.radioButton_count1))
+        self.ui.radioButton_count2.clicked.connect(lambda: self.getChangingCount(self.ui.radioButton_count2))
+
+        self.ui.pushButton_request.clicked.connect(self.order)
+
+    def getChangingDate(self):
+        global user_date
+        user_date = self.ui.dateEdit.date().toString('yyyy-MM-dd')
+        print(user_date)
+
+    def getChangingType(self, btn):
+        global user_type
+        user_type = btn.text()
+        print(user_type)
+
+    def getChangingCount(self, btn):
+        global user_count
+        user_count = btn.text()
+        print(user_count)
 
     def test(self):
         print('has !')
 
     def order(self):
-        user_auth = self.ui.plainTextEdit.toPlainText()
-        user_date = self.ui.plainTextEdit_2.toPlainText()
-        user_type = self.ui.plainTextEdit_4.toPlainText()
-        user_count = self.ui.plainTextEdit_5.toPlainText()
-        print(user_auth)
-        print(user_date)
-        print(user_type)
-        print(user_count)
+        global user_date, user_type, user_count
+        user_auth = self.ui.user_auth.toPlainText()
+
         autobook.get_info(user_auth, user_date, user_type, user_count)
         # print(autobook.get_all_stadium())
         # print(autobook.find_free_field())
